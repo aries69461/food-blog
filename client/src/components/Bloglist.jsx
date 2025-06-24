385afd7eba8b79c35d006b8f5ddee58dd2ba7cfe
aries69461 <ariesrance69@gmail.com>
2025-06-24 18:44:44 +0800

    edit home response

--- client/src/components/Bloglist.jsx
+++ client/src/components/Bloglist.jsx
@@ -17,10 +17,10 @@ const Bloglist = () => {
 
   return (
     <div>
-      <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
+      <div className='flex flex-wrap justify-center gap-x-4 gap-y-2 sm:gap-8 my-10 relative px-4 sm:px-8'>
         {blogCategories.map((item)=>(
           <div key={item} className='relative'>
-            <button onClick={()=> setMenu(item)} className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>
+            <button onClick={()=> setMenu(item)} className={`cursor-pointer text-gray-500 text-sm py-1.5 px-3 rounded-full ${menu === item && 'text-white px-4 pt-0.5'}`}>
               {item}
               {menu === item && (
                 <motion.div layoutId='underline' transition={{type:'spring', stiffness:500, damping:30}} className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></motion.div>
